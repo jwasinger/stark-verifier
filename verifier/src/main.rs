e120a7553c1e2ff957ca291ab0eb022bb5d45dcd
Jared Wasinger <j-wasinger@hotmail.com>
2019-06-07 21:41:02 +0000

    f

--- verifier/src/main.rs
+++ verifier/src/main.rs
@@ -1,7 +1,39 @@
-fn verify() {
+/*
+Proof:
+    merkle roots of P, D, spot check merkle proofs, low-degree proofs of P and D
+*/
 
-}
+use std::sync::Arc;
+use rustfft::FFTplanner;
+use rustfft::num_complex::Complex;
+use rustfft::num_traits::Zero;
 
 fn main() {
-    println!("Hello, world!");
+    let mut input:  Vec<Complex<f32>> = vec![Complex::zero(); 1234];
+    let mut output: Vec<Complex<f32>> = vec![Complex::zero(); 1234];
+
+    let mut planner = FFTplanner::new(false);
+    let fft = planner.plan_fft(1234);
+    fft.process(&mut input, &mut output);
+     
+    // The fft instance returned by the planner is stored behind an `Arc`, so it's cheap to clone
+    let fft_clone = Arc::clone(&fft);
+}
+
+const EXTENSION_FACTOR: u32 = 8;
+
+fn verify_mimc_proof(inp: u32, num_steps: u32, round_constants: &[u32], output: u32, proof: Proof) -> bool {
+    if num_steps > floor(pow(2, 32) / EXTENSION_FACTOR) {
+        false
+    }
+
+    if !is_a_power_of_2(steps) || !is_a_power_of_2(round_constants.len()) {
+        false
+    }
+
+    if round_constants.len() < num_steps {
+        false
+    }
+
+    true
 }
